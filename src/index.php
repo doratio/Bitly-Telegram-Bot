df988bb9632f32c8582fedecd599e1de5bb35d05
Aidar Akhmetow <doratio@MacBook-Pro-Aidar.local>
2018-01-04 02:14:07 +0300

    add method create keyboards

--- src/index.php
+++ src/index.php
@@ -1,9 +1,12 @@
 <?php
+//https://api.telegram.org/bot380289834:AAGjd6Q0j5F_1hToz92I4bJnKK94gmQNl00/
+//sendmessage?text=hello&chat_id=276921476
+//&reply_markup={%22resize_keyboard%22:true,%22keyboard%22:[[%22%D0%9F%D1%80%D0%B8%D0%B2%D0%B5%D1%82%22],[%22%D0%9F%D0%BE%D0%BA%D0%B0%22]]}
 require('telegram/Bot.php');
 require('Log/FLogger.php');
 
 $log = new FLogger("log.txt");
-$bot = new \telegram\Bot();
+$bot = new \telegram\Bot($log);
 
 $log->log("запрос на обновление");
 
@@ -17,6 +20,15 @@ $log->log("id чата: ".$chat_id);
 
 $log->log("запрос прошел");
 
+
 $log->log("отправляем сообщение");
-$bot->sendMessage("hello", $chat_id);
+//$bot->sendMessage("hello", $chat_id);
 $log->log("сообщение отправлено");
+
+$log->log("создание клавиатуры");
+$keyboards = [["При"], ["Пока"]];
+$keyboardSettings = array(
+    "resize_keyboard" => true,
+);
+$bot->keyboard(" f", $chat_id, $keyboards, $keyboardSettings);
+$log->log("клавиатура создана");
