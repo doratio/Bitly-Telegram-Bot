da797d96448342dbbdb6df4a75380335c2d90ab3
Aidar Akhmetow <doratio@MacBook-Pro-Aidar.local>
2018-01-04 17:50:37 +0300

    created bot's logic

--- src/index.php
+++ src/index.php
@@ -1,55 +1,19 @@
 <?php
-//https://api.telegram.org/bot380289834:AAGjd6Q0j5F_1hToz92I4bJnKK94gmQNl00/
-//sendmessage?text=hello&chat_id=276921476
-//&reply_markup={%22resize_keyboard%22:true,%22keyboard%22:[[%22%D0%9F%D1%80%D0%B8%D0%B2%D0%B5%D1%82%22],[%22%D0%9F%D0%BE%D0%BA%D0%B0%22]]}
+
 require('telegram/Bot.php');
-require('Log/FLogger.php');
 
-$log = new FLogger("Log/log.txt");
-$bot = new \telegram\Bot($log);
-
-$log->log("запрос на обновление");
-
-$result = (array)$bot->getUpdates();
-echo "<pre>";
-var_dump($result);
-echo "</pre>";
-
-$chat_id = ((array)((array)((array)$result["result"][count($result["result"])-1])["message"])["chat"])["id"];
-$log->log("id чата: ".$chat_id);
-
-$log->log("запрос прошел");
+$bot = new \telegram\Bot();
+$bot->run();
 
 
-$log->log("отправляем сообщение");
-$bot->sendMessage("hello", $chat_id);
-$log->log("сообщение отправлено");
 
-$log->log("создание клавиатуры");
-$keyboards = [["При"], ["Пока"]];
-$keyboardSettings = array(
-    "resize_keyboard" => true,
-);
-$bot->keyboard(" f", $chat_id, $keyboards, $keyboardSettings);
-$log->log("клавиатура создана");
-
-$log->log("создание ярлыка");
-
-$inlineKeyboards = [
-    "inline_keyboard" => [
-        [
-            (object)[
-                "text" => "<",
-                "callback_data" => "google.com"
-
-            ],
-            (object)[
-                "text" => ">",
-                "callback_data" => "google.com"
-            ]
-        ]
-    ]
-];
-
-$bot->inlineKeyboard(" f", $chat_id, $inlineKeyboards);
-$log->log("ярлык создан");
+//
+//$log->log("создание клавиатуры");
+//$keyboards = [["При"], ["Пока"]];
+//$keyboardSettings = array(
+//    "resize_keyboard" => true,
+//);
+//$bot->keyboard(" f", $chat_id, $keyboards, $keyboardSettings);
+//$log->log("клавиатура создана");
+//
+;
--- src/telegram/Bot.php
+++ src/telegram/Bot.php
@@ -1,30 +1,43 @@
 <?php
 namespace telegram;
 
-require('http/Connection.php');
+require('telegram/Connection.php');
+require('Log/FLogger.php');
+require('TIniFileEx.php');
 
 class Bot
 {
 
     private $connection;
     private $log;
+    private $config;
+    private $configTelegram;
+    private $lastupdate;
+
     /**
      * Bot constructor.
      */
-    public function __construct($log)
+    public function __construct()
     {
-        $this->log = $log;
-        $telegramConfig = parse_ini_file("telegram/config.ini", false);
+        $this->config = new \TIniFileEx("config.ini");
+        $this->configTelegram = new \TIniFileEx("telegram/config.ini");
 
-        $url = $telegramConfig['URL'];
-        $token = $telegramConfig['token'];
+        $this->log = new \FLogger($this->config->read('main', "logFile"));
+        $this->lastupdate = $this->config->read('main', "lastUpdateID");
 
-        $this->connection = new \Connection($token, $url);
+        $this->connection = new Connection(
+            $this->configTelegram->read('telegram', 'token'),
+            $this->configTelegram->read('telegram', 'URL')
+        );
     }
 
-    public function getUpdates()
+    public function getUpdates($offset)
     {
-        return $this->connection->request("getUpdates",null);
+        $params = null;
+        if (!empty($offset)) {
+            $params["offset"] = $offset;
+        }
+        return $this->connection->request("getUpdates", $params);
     }
 
     public function sendMessage($text, $chatID)
@@ -55,5 +68,84 @@ class Bot
         $params["reply_markup"] = json_encode($inlineKeyboards);
 
         $this->connection->request("sendmessage", $params);
+    }
+
+    public function run()
+    {
+        while (true) {
+            $this->log->log("запрос на обновление");
+            $updates = ((array)$this->getUpdates($this->lastupdate))["result"];
+            $this->log->log("запрос прошел");
+
+            echo "<pre>";
+            var_dump($updates);
+            echo "</pre>";
+            $chat_id = null;
+            $chat = null;
+            $message = null;
+            $update_id = null;
+
+            foreach ($updates as $update) {
+                $update_id = ((array)$update)["update_id"];
+                $chat = ((array)((array)$update)["message"])["chat"];
+                $chat_id = ((array)$chat)["id"];
+                $this->log->log("id чата: " . $chat_id);
+                $message = ((array)((array)$update)["message"])["text"];
+
+                switch ($message) {
+                    case "/start":
+                        $this->log->log("создание клавиатуры");
+                        $keyboards = [["История"], ["Помощь"]];
+                        $keyboardSettings = array(
+                            "resize_keyboard" => true,
+                        );
+                        $this->keyboard(" f", $chat_id, $keyboards, $keyboardSettings);
+                        $this->log->log("клавиатура создана");
+                        break;
+
+                    case "История":
+                        $this->log->log("создание ярлыка");
+
+                        $inlineKeyboards = [
+                            "inline_keyboard" => [
+                                [
+                                    (object)[
+                                        "text" => "<",
+                                        "callback_data" => "google.com"
+                                    ],
+                                    (object)[
+                                        "text" => ">",
+                                        "callback_data" => "google.com"
+                                    ]
+                                ]
+                            ]
+                        ];
+
+                        $this->inlineKeyboard("История", $chat_id, $inlineKeyboards);
+                        $this->log->log("ярлык создан");
+                        break;
+
+                    case "/help":
+                    case "Помощь":
+                        $this->log->log("отправляем сообщение");
+                        $this->sendMessage("помощь", $chat_id);
+                        $this->log->log("сообщение отправлено");
+                        break;
+
+                    default:
+                        $this->log->log("отправляем сообщение");
+                        $this->sendMessage("text", $chat_id);
+                        $this->log->log("сообщение отправлено");
+                }
+            }
+
+            $this->lastupdate = $update_id+1;
+            if ($this->lastupdate > 1) {
+                $this->config->write('main', "lastUpdateID", $this->lastupdate);
+                $this->config->updateFile();
+            }
+
+            sleep(1);
+        }
     }
 }
